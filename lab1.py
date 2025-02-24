a8dbbab3b23e96c665f05cf493fa919f2b8764cc
ΚΩΝΣΤΑΝΤΟΥΡΑΣ ΓΕΩΡΓΙΟΣ <up1092678>
2025-02-24 12:48:26 +0200

    printing headers

--- lab1.py
+++ lab1.py
@@ -23,6 +23,5 @@ if not url.startswith("https://"):
 print(url)
 
 with requests.get(url) as response:
-    print(response.text)
-
-print("hi")
+    for key in response.headers:
+        print(f"{key}:{response.headers[key]}")
